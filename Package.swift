957ebe15cc7f6dd3ebe45b3ed83557dafc35e620
Cimons <953330953@qq.com>
2021-04-06 19:27:27 +0800

    add SPM

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PulsingHalo",
+    products: [
+        .library(
+            name: "PulsingHalo",
+            targets: ["PulsingHalo"]),
+    ],
+    targets: [
+        .target(
+            name: "PulsingHalo",
+            dependencies: [],
+            path: ".",
+            sources: ["PulsingHaloLayer.h",
+                      "PulsingHaloLayer.m"],
+            publicHeadersPath: "include")
+        
+    ]
+)
---
+++ include/PulsingHaloLayer.h
@@ -0,0 +1,1 @@
+../PulsingHaloLayer.h
